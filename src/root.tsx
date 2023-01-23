15ef46728966b95ea5cdb58a83e26f72046f1dfa
rayhan <rayhan.rhssoe2@gmail.com>
2023-01-23 16:28:15 +0700

    error boundary.

--- src/root.tsx
+++ src/root.tsx
@@ -2,35 +2,47 @@
 import "./root.css";
 import { Suspense } from "solid-js";
 import {
-  Body,
-  ErrorBoundary,
-  FileRoutes,
-  Head,
-  Html,
-  Meta,
-  Routes,
-  Scripts,
-  Title,
+	Body,
+	ErrorBoundary,
+	FileRoutes,
+	Head,
+	Html,
+	Meta,
+	Routes,
+	Scripts,
+	Title,
 } from "solid-start";
 
 export default function Root() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Title>Create JD App</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
-  );
+	return (
+		<Html lang='en'>
+			<Head>
+				<Title>Create JD App</Title>
+				<Meta charset='utf-8' />
+				<Meta name='viewport' content='width=device-width, initial-scale=1' />
+			</Head>
+			<Body>
+				<Suspense>
+					<ErrorBoundary
+						fallback={(e: Error) => (
+							<>
+								<h2>Oh no! An Error!</h2>
+								<details>
+									<summary>Click here to learn more</summary>
+									<p>
+										<strong>{e.name}</strong>: {e.message}
+									</p>
+								</details>
+							</>
+						)}
+					>
+						<Routes>
+							<FileRoutes />
+						</Routes>
+					</ErrorBoundary>
+				</Suspense>
+				<Scripts />
+			</Body>
+		</Html>
+	);
 }
