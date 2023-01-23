a0063e0ebd7fdf91fa2837166986fcac65c2f550
rayhan <rayhan.rhssoe2@gmail.com>
2023-01-23 22:59:31 +0700

    types.

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export interface CartItemProps {
+	id: string;
+	quantity: number;
+	productId: string;
+	isChecked: boolean;
+	status: boolean;
+	updatedAt?: Date;
+	createdAt?: Date;
+}
+
+export interface ProductProps {
+	id: string;
+	name: string;
+	category: string;
+	stock: number;
+	price: number;
+	imgUrl: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+	popularity: number;
+}
+
+export interface TransactionProps {
+	id: string;
+	updatedAt?: Date;
+	createdAt?: Date;
+	totalPrice: number;
+}
+
+export interface TransactionItemProps {
+	id: string;
+	transactionId: string;
+	quantity: number;
+	productId: string;
+	updatedAt?: Date;
+	createdAt?: Date;
+}
